82f389cbec0edecc0d2e6af98d85ec848d8dc9f5
Kingsley <k@n.com>
2020-02-05 14:03:55 +0100

    removed answers

--- js/master.js
+++ js/master.js
@@ -1,8 +1,3 @@
 // select element from the DOM
-const button = document.getElementById("btn");
 
 // add event "click" event listner to the button.
-// the function passed in will be invoked when the event happens
-button.addEventListener("click", function() {
-    console.log("Clicked");
-});
